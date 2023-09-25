1a4d56bf80a9648db225364f6e7b50d4d5c5e185
lzc <lvzicong123@outlook.com>
2023-09-25 16:14:00 +0800

    博客列表搭建

--- src/view/Admin/BlogList/index.tsx
+++ src/view/Admin/BlogList/index.tsx
@@ -1,8 +1,72 @@
 import React from 'react';
+import { Table, Button } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import { SearchOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
+
+interface DataType {
+    key: React.Key;
+    name: string;
+    age: number;
+    address: string;
+}
+const columns: ColumnsType<DataType> = [
+    {
+        title: 'Full Name',
+        width: 100,
+        dataIndex: 'name',
+        key: 'name',
+        fixed: 'left',
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    { title: 'Column 1', dataIndex: 'address', key: '1' },
+    { title: 'Column 2', dataIndex: 'address', key: '2' },
+    { title: 'Column 3', dataIndex: 'address', key: '3' },
+    { title: 'Column 4', dataIndex: 'address', key: '4' },
+    { title: 'Column 5', dataIndex: 'address', key: '5' },
+    { title: 'Column 6', dataIndex: 'address', key: '6' },
+    { title: 'Column 7', dataIndex: 'address', key: '7' },
+    { title: 'Column 8', dataIndex: 'address', key: '8' },
+    {
+        title: 'Action',
+        key: 'operation',
+        fixed: 'right',
+        width: 250,
+        render: () => {
+            return (
+                <span style={{ display:'flex', justifyContent:'space-around' }}>
+                    <Button type="primary" icon={<SearchOutlined />} size='small' shape="default">查看</Button>
+                    <Button type="primary" style={{ backgroundColor:'#EFAE2A' }} icon={<EditOutlined />} size='small' shape="default">编辑</Button>
+                    <Button type="primary" danger icon={<DeleteOutlined />} size='small' shape="default">删除</Button>
+                </span>
+            )
+        },
+    },
+];
+
+const data: DataType[] = [
+    {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York Park',
+    },
+    {
+        key: '2',
+        name: 'Jim Green',
+        age: 40,
+        address: 'London Park',
+    },
+];
 
 const AdminBlogList: React.FC = () => {
     return (
-        <div>AdminBlogList</div>
+        <div>
+            <Table columns={columns} dataSource={data} scroll={{ x: 1300 }} />
+        </div>
     )
 }
 export default AdminBlogList;
