8671c576c1c1aadd4557ce81460976e17c8f515d
lzc <lvzicong123@outlook.com>
2023-10-09 17:58:17 +0800

    博客管理

--- src/view/Admin/Home/index.tsx
+++ src/view/Admin/Home/index.tsx
@@ -1,14 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Component1 from './component/component1';
 
 const AdminHome: React.FC = () => {
 
+    const [time,setTime] = useState<boolean>(false);
+
+    const sleep = (timeout:number):Promise<any>=> {
+        let flag = new Promise((reslove:any,reject:any)=>{
+            setTimeout(()=>{
+                reslove('执行了');
+            },timeout)
+        })
+        return flag
+    };
+
     useEffect(()=>{
+        sleep(5000).then((res)=>{
+            console.log(res);
+        });
         console.log('父组件渲染了');
         return () => {
             console.log('父组件卸载了');
         }
-        
     },[])
 
     return (
